a2af24ea5367a8913cabc2ccf3910be401ac7433
MurdocAV <azaan.virk1@gmail.com>
2019-10-01 13:29:05 +1300

    Renamed and using m-ui components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Calculation from './Calculation'
+import TabHeader from './Tab'
 
 class App extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <Calculation></Calculation>
-        {/* <Calculation></Calculation>
-        <Calculation></Calculation>
-        <Calculation></Calculation> */}
+        <TabHeader></TabHeader>
       </React.Fragment>
     )
   }
